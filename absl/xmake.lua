1b6cba5938d60ff5b4d5cff6ad385304bd09d151
Maxwell Geng <maxwellgeng@outlook.com>
2022-10-13 18:19:47 +0800

    xmake configure

--- absl/xmake.lua
+++ absl/xmake.lua
@@ -1,11 +1,15 @@
 BuildProject({
-    projectName = "abseil_dll",
-    projectType = "shared"
+	projectName = "abseil_dll",
+	projectType = "shared"
 })
 add_defines("ABSL_BUILD_DLL=1")
-add_defines("ABSL_CONSUME_DLL=1",  "NOMINMAX", {public = true})
+add_defines("ABSL_CONSUME_DLL=1", "NOMINMAX", {
+	public = true
+})
 add_files("**.cc")
 --[[
    
 ]]
-add_includedirs("../", {public = true})
+add_includedirs("../", {
+	public = true
+})
